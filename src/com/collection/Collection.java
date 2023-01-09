7f0fff0a95ae0cb871fab546291e7fbd03a26326
aishwarya <aishukharde991@gmail.com>
2023-01-08 18:02:06 -0800

    Added ArrayList Demo

---
+++ src/com/collection/Collection.java
@@ -0,0 +1,25 @@
+package com.collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class Collection {
+
+	
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Ashwini");
+		list.add("priya");
+		list.add("shwet");
+		list.add("shankar");
+		
+		for(String s :list) {
+			System.out.println(s);
+		}
+		
+	}
+}
